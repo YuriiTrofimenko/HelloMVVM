024d2d9270cba23dc2d43f16024ac4b92161dfb4
YuriiTrofimenko <tyaa@ukr.net>
2020-04-29 10:43:42 +0300

    MultiBinding

--- HelloMVVM/ViewModels/MainWindowViewModel.cs
+++ HelloMVVM/ViewModels/MainWindowViewModel.cs
@@ -8,13 +8,16 @@
     public class MainWindowViewModel : ViewModelBase
     {
         public static readonly PropertyData FirstNameProperty;
+        public static readonly PropertyData LastNameProperty;
         public Command ShowFirstName { get; private set; }
         static MainWindowViewModel()
         {
             FirstNameProperty
                 = RegisterProperty(nameof(FirstName), typeof(string), null);
+            LastNameProperty
+                = RegisterProperty(nameof(LastName), typeof(string), null);
         }
-
+        
         public MainWindowViewModel()
         {
             ShowFirstName = new Command(OnShowFirstNameExecute, OnShowFirstNameCanExecute);
@@ -24,13 +27,39 @@
 
         public string FirstName
         {
-            get { return GetValue<string>(FirstNameProperty); }
+            get {
+                if (IsCheked)
+                    return GetValue<string>(FirstNameProperty);
+                else
+                    return "";
+            }
             set { SetValue(FirstNameProperty, value); }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                if (IsCheked)
+                    return GetValue<string>(LastNameProperty);
+                else
+                    return "";
+            }
+            set { SetValue(LastNameProperty, value); }
         }
 
         // TODO: Register models with the vmpropmodel codesnippet
         // TODO: Register view model properties with the vmprop or vmpropviewmodeltomodel codesnippets
         // TODO: Register commands with the vmcommand or vmcommandwithcanexecute codesnippets
+
+
+        public bool IsCheked
+        {
+            get { return GetValue<bool>(IsChekedProperty); }
+            set { SetValue(IsChekedProperty, value); }
+        }
+
+        public static readonly PropertyData IsChekedProperty = RegisterProperty(nameof(IsCheked), typeof(bool), null);
 
         private void OnShowFirstNameExecute()
         {
@@ -39,8 +68,10 @@
 
         private bool OnShowFirstNameCanExecute()
         {
-            return true;
+            return IsCheked;
         }
+
+
 
         protected override async Task InitializeAsync()
         {
